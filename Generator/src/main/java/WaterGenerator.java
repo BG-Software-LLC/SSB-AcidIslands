f114bf42a9f04154360dd18d4f3b4e5b92c8fba1
OmerBenGera <omerthepro126@gmail.com>
2021-03-28 20:13:56 +0300

    Added gradle support

--- Generator/src/main/java/WaterGenerator.java
+++ Generator/src/main/java/WaterGenerator.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-@SuppressWarnings({"deprecation", "unused", "NullableProblems"})
+@SuppressWarnings({"deprecation", "unused"})
 public final class WaterGenerator extends ChunkGenerator {
 
     private final double islandsLevel;
